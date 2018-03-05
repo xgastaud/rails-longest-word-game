c8c9fb7802c8e6db3299fd0c4fffff7e005eb25c
Xavier Gastaud <xgastaud@gmail.com>
2018-03-05 22:31:00 +0100

    updated controller to prevent reuse of a letter

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -11,25 +11,30 @@ class GamesController < ApplicationController
   end
 
   def score
-    session[:total_score] = 0
+    # @total = 0
+    # session[:total_score] = 0
+    if session[:total_score].nil?
+      session[:total_score] = 0
+    end
     @score = 0
     @word = params[:word].upcase
     @rand_array = params[:rand_array]
-    @word.split("").each do |letter|
-      if @rand_array.include?(letter)
-      # if word can't be built out of original grid
-        url_result = "https://wagon-dictionary.herokuapp.com/#{@word}"
-        serialized_results = open(url_result).read
-        results = JSON.parse(serialized_results)
-        if results["found"] == true
-          # if word is a valid English word, increment score
-          @score = @word.length
-          @message = "Good answer!"
-          session[:total_score] += @score
-        end
+    if @word.split("").all? do |letter|
+        @rand_array.include?(letter) && @word.count(letter) <= @rand_array.count(letter)
+      end
+      url_result = "https://wagon-dictionary.herokuapp.com/#{@word}"
+      serialized_results = open(url_result).read
+      results = JSON.parse(serialized_results)
+      if results["found"] == true
+        # if word is a valid English word, increment score
+        @score = @word.length
+        @message = "Good answer!"
+        session[:total_score] += @score
       else
         @message = "invalid answer!"
       end
+    else
+      @message = "invalid answer!"
     end
     @total = session[:total_score]
   end
